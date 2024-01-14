9314ed23510c23a6f7cfc86757d30fec3426898b
KlaraMartinsson <>
2024-01-14 23:07:48 +0000

    Enable serving of static files

--- world_wide_writers/settings.py
+++ world_wide_writers/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-q%^^51s=1-#ksv4&nd0=0e*_8bjey6k6pv5%=&2w!mv8!!%-9_
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["8000-klaramartinsson-worldwid-tfe3cmjrej.us2.codeanyapp.com", '.herokuapp.com']
 
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,6 +134,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
