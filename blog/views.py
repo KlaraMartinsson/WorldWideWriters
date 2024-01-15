7890c94507c07de2f4b8bf01af63867a935de91f
KlaraMartinsson <>
2024-01-15 14:08:26 +0000

    Add post_detail.html to view post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -7,3 +7,14 @@ from .models import Post
 class HomePage(generic.ListView):
     queryset = Post.objects.all()
     template_name = "blog/index.html"
+
+
+def post_detail(request, slug):
+    queryset = Post.objects.all()
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
