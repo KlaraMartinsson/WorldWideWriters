42c168d56c0b9792d2f9be672355c1244605da6f
KlaraMartinsson <>
2024-01-17 12:48:33 +0000

    Add a feature for user to create posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
+    path('user_post/', views.user_post, name='user_post'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.utils.text import slugify
 from .models import Post
+from .forms import PostForm
 
 
 # Create your views here.
@@ -17,4 +19,24 @@ def post_detail(request, slug):
         request,
         "blog/post_detail.html",
         {"post": post},
+    )
+
+
+def user_post(request):
+    if request.method == "POST":
+        post_form = PostForm(data=request.POST)
+        if post_form.is_valid():
+            post = post_form.save(commit=False)
+            post.author = request.user
+            post.slug = slugify(post.title)
+            post.save()
+            
+    post_form = PostForm()
+            
+    return render(
+        request,
+        "blog/user_post.html",
+        {
+            "post_form": post_form,
+        },
     )
